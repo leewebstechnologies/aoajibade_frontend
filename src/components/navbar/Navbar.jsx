f1e062df270fc0ce857ce94669790f0316160914
leewebstechnologies <aliuajibade317@gmail.com>
2023-10-04 23:02:58 +0100

    menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import "./navbar.css";
+import "./navbar.scss";
 import { images } from "../../constants";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { easeOut, motion } from "framer-motion";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -15,6 +19,18 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: easeOut }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
