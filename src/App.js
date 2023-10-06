c34116e31d9d4545ff00f4b39152135295ab8907
leewebstechnologies <aliuajibade317@gmail.com>
2023-10-06 22:22:05 +0100

    dotenv

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./app.scss";
 
+
 function App() {
   return (
     <div className="app">
